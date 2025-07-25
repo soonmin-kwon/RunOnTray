2e8d59969f6118f82f523f5b369a1b56c700ce61
sunmin97 <tnsals0709@naver.com>
2025-07-26 00:15:31 +0900

    Initialize Setting
    - push default solution files
    - Set Tray Icon
      - make MainWindow not open
      - add contextMenu to Tray Icon
      - change icon resoruce on timer interval
    - create CpuUsageTimer
      - timer's interval is affected by cpu usage
      - get cpu usage from % processor utility, 0,_Total
      - use thread for dynamic interval

---
+++ App.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace RunOnTray
+{
+    public partial class App : System.Windows.Application
+    {
+        public int runAnimLength = 8;
+        private NotifyIcon trayIcon = null;
+        private List<Icon> icons = null;
+        private int currentIndex = 0;
+
+        private CpuUsageTimer timer = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //MainWindow = new MainWindow();
+            //MainWindow.Topmost = false;
+            //MainWindow.Hide(); // 윈도우 숨기기 (트레이 앱용)
+            //MainWindow.Visibility = Visibility.Hidden;
+
+            SetIcons();
+
+            // 트레이 아이콘 설정
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = icons[currentIndex];
+            trayIcon.Visible = true;
+            trayIcon.Text = "RunOnTray";
+            
+            // 트레이 메뉴
+            var contextMenu = new ContextMenuStrip();
+            SetTurtleContextMenu(contextMenu);
+            SetPerformanceCounter();
+
+            trayIcon.ContextMenuStrip = contextMenu;
+        }
+
+
+        private void SetIcons()
+        {
+            icons = new List<Icon>(runAnimLength);
+
+            for (int i=0; i< runAnimLength; i++)
+            {
+                icons.Add(new Icon(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "resources", $"{(i + 1).ToString()}.ico")));
+            }
+        }
+
+        private void SetTurtleContextMenu(ContextMenuStrip contextMenu)
+        {
+            var exitItem = new ToolStripMenuItem("Exit");
+            exitItem.Click += (s, args) =>
+            {
+                trayIcon.Visible = false;
+                Shutdown();
+            };
+
+            
+            // 설정 누르면 새 윈도우 열리고 거기서 아이콘 path 설정
+            // 아이콘 path 설정 시 아이콘 리셋
+            var speedUpItem = new ToolStripMenuItem("Speed Up");
+            speedUpItem.Click += (s, args) =>
+            {
+                if (timer != null)
+                {
+                    timer.SetInterval(true);
+                }
+            };
+
+            var slowDownItem = new ToolStripMenuItem("Slow Down");
+            slowDownItem.Click += (s, args) =>
+            {
+                if (timer != null)
+                {
+                    timer.SetInterval(false);
+                }
+            };
+
+            contextMenu.Items.Add(exitItem);
+            contextMenu.Items.Add(speedUpItem);
+            contextMenu.Items.Add(slowDownItem);
+        }
+
+        private void SetPerformanceCounter()
+        {            
+            timer = new CpuUsageTimer();
+
+            timer.Tick += (s, args) =>
+            {
+                if (trayIcon != null)
+                {
+                    if (currentIndex > runAnimLength - 1)
+                    {
+                        currentIndex = 0;
+                    }
+
+                    trayIcon.Icon = icons[currentIndex];
+                    currentIndex += 1;
+                }
+            };
+
+            timer.Start();
+        }
+    }
+}
---
+++ CpuUsageTimer.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+namespace RunOnTray
+{
+    public class CpuUsageTimer
+    {
+        private const float WEIGHT = 10f;
+
+        private PerformanceCounter cpuUsageCounter = new PerformanceCounter("Processor Information", "% Processor Utility", "0,_Total");
+        private Thread timerThread = null;
+        private float DEFAULT_INTERVAL = 500f;
+        
+        public event EventHandler? Tick;
+
+        public CpuUsageTimer()
+        {
+            timerThread = new Thread(Run);
+            timerThread.Name = "CPUUSageTimer";
+            timerThread.IsBackground = true;
+        }
+
+        private void Run()
+        {
+            float interval;
+            float cpuUsage = cpuUsageCounter.NextValue();
+            while (true)
+            {
+                if (Tick == null)
+                {
+                    Thread.Yield();
+                }
+
+                cpuUsage = (cpuUsageCounter.NextValue());
+
+                if (cpuUsage < 10)
+                {
+                    interval = DEFAULT_INTERVAL;
+                }
+                else
+                {
+                    interval = DEFAULT_INTERVAL / (cpuUsage / WEIGHT);
+                }
+                
+                Tick?.Invoke(null, null);
+
+                Thread.Sleep((int)(interval));
+            }
+        }
+
+        public void Start()
+        {
+            timerThread.Start();
+        }
+
+
+        public void SetInterval(bool speedUp)
+        {
+            if (speedUp)
+            {
+                DEFAULT_INTERVAL = Math.Max(200, DEFAULT_INTERVAL - 100);
+            }
+            else
+            {
+                DEFAULT_INTERVAL = Math.Min(1000, DEFAULT_INTERVAL + 100);
+            }
+        }
+
+        public Thread GetTimerThread()
+        {
+            return timerThread;
+        }
+    }
+}
